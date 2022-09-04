79b0b916abfb2da3bc52f642b5cfb0f1f20c9195
live-young <61771639+live-young@users.noreply.github.com>
2022-09-04 20:01:45 +0530

    refactoring test class

--- src/test/java/ReadingRangeCountTest.java
+++ src/test/java/ReadingRangeCountTest.java
@@ -1,17 +1,25 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNull;
+
+import java.util.List;
+
 import org.junit.Test;
-import java.util.*;
-import static org.junit.Assert.*;
-
-public class ReadingRangeCountTest {
-    ReadingRangeCount readingRangeCount = new ReadingRangeCount();
-
-    @Test
-    public void countGivenRangeTest_invalid_startRange() throws Exception {
-    	List <Integer> inputRange = new ArrayList<>(Arrays.asList(4,5));
-    	Map<String,Integer> result = readingRangeCount.getChargingRange(inputRange);
-    	assertTrue(!result.isEmpty());
-    	assertTrue(result.get("4 to 5")==2);
-        
-    }
-
+public class ReadRangeCountTest {
+  @Test
+  public void testRange() {
+    assertNull(ReadingRangeCount.findNoOfReadingsInRange());
+    assertNull(ReadingRangeCount.findNoOfReadingsInRange(4));
+    assertNotEquals("4-5,3", ReadingRangeCount.findNoOfReadingsInRange(4, 5).get(0));
+    assertEquals("4-5,2", ReadingRangeCount.findNoOfReadingsInRange(4, 5).get(0));
+    assertEquals("4-7,4", ReadingRangeCount.findNoOfReadingsInRange(4, 5, 6, 7).get(0));
+    List<String> actualOutput = ReadingRangeCount.findNoOfReadingsInRange(3, 3, 5, 4, 10, 11, 12);
+    assertEquals("3-5,4", actualOutput.get(0));
+    assertEquals("10-12,3", actualOutput.get(1));
+    List<String> actualOutput1 = ReadingRangeCount.findNoOfReadingsInRange(5, 4, 10, 11, 12);
+    assertEquals("4-5,2", actualOutput1.get(0));
+    assertEquals("10-12,3", actualOutput1.get(1));
+    List<String> actualOutput2 = ReadingRangeCount.findNoOfReadingsInRange(4, 10, 11, 12);
+    assertEquals("10-12,3", actualOutput2.get(0));
+  }
 }
